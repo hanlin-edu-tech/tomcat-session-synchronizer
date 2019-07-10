527a19a13cb891db61a4920226e04f8acaca4112
莊季儒（Aaron） <chuang.chi.ju@gmail.com>
2019-07-10 09:50:38 +0800

    新增submodule for micronaut support(TODO:unit test)

---
+++ micronaut/src/main/java/tw/com/ehanlin/micronautSessionSynchronizer/micronaut/MicronautSessionStoreSynchronizer.java
@@ -0,0 +1,43 @@
+package tw.com.ehanlin.micronautSessionSynchronizer.micronaut;
+
+import io.micronaut.context.annotation.Primary;
+import io.micronaut.context.event.ApplicationEventPublisher;
+import io.micronaut.session.InMemorySession;
+import io.micronaut.session.InMemorySessionStore;
+import io.micronaut.session.SessionConfiguration;
+import io.micronaut.session.SessionIdGenerator;
+import tw.com.ehanlin.tomcatSessionSynchronizer.core.Synchronizer;
+
+import javax.inject.Singleton;
+
+@Singleton
+@Primary
+public class MicronautSessionStoreSynchronizer extends InMemorySessionStore {
+
+    private final SessionConfiguration sessionConfiguration;
+    private final SessionIdGenerator sessionIdGenerator;
+    private Synchronizer _synchronizer;
+    public void setSynchronizer(Synchronizer synchronizer){
+        this._synchronizer = synchronizer;
+    }
+    public Synchronizer getSynchronizer(){
+        return this._synchronizer;
+    }
+
+    public MicronautSessionStoreSynchronizer(
+            SessionIdGenerator sessionIdGenerator,
+            SessionConfiguration sessionConfiguration,
+            ApplicationEventPublisher eventPublisher,
+            Synchronizer synchronizer) {
+        super(sessionIdGenerator, sessionConfiguration, eventPublisher);
+        this._synchronizer = synchronizer;
+        this.sessionIdGenerator = sessionIdGenerator;
+        this.sessionConfiguration = sessionConfiguration;
+    }
+
+    @Override
+    public InMemorySession newSession() {
+        return new SynchronizableInMemorySession(sessionIdGenerator.generateId(), sessionConfiguration.getMaxInactiveInterval(),
+                this._synchronizer);
+    }
+}
